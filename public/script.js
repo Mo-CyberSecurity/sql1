4ca290e5bdcc70bf6c2f9963aefbc3a0faf43523
Momavi <68790545+Momavi@users.noreply.github.com>
2022-04-27 09:12:52 +0300

    a

--- public/script.js
+++ public/script.js
@@ -1,4 +1,36 @@
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+function changeCompany(text) {
+  document.getElementById("input").value = text;
+}
+
+function redirect() {
+  let inputValue = document.getElementById("input").value
+  let phoneElement = document.querySelector('.phone')
+  let messageElement = document.getElementById("message")
+  if (inputValue) {
+    fetch('/api/?company=' + inputValue)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.status === 200) {
+          phoneElement.innerHTML = ''
+          for (let i = 0; i < data.data.length; i++) {
+            for (key in data.data[i]) {
+              let row = document.createElement('tr')
+              row.innerHTML = `<td coldspan="2">${key
+                } - ${data.data[i][key]}</td>`
+              phoneElement.appendChild(row)
+            }
+            let row = document.createElement('tr')
+            phoneElement.appendChild(document.createElement('div'))
+          }
+          console.log(data)
+          messageElement.innerHTML = data.data.length
+        }
+        if (data.status === 404) {
+          messageElement.innerHTML = data.data.sqlMessage
+          console.log(data)
+        }
+      });
+  }
 }
--- server.js
+++ server.js
@@ -3,15 +3,39 @@ const express = require("express"),
   path = require("path"),
   mysql = require('mysql2');
 
-const port = process.env.YOUR_PORT || process.env.PORT || 8080;
-const flag = "flag{}";
+const port = process.env.YOUR_PORT || process.env.PORT || 33341;
+
+// const conn = mysql.createConnection({
+//   host: "77.77.77.74",
+//   port: "3306",
+//   user: "user",
+//   database: "mydb",
+//   password: "password"
+// });
 
 const conn = mysql.createConnection({
-  host: "77.77.77.74",
+  host: "localhost",
   port: "3306",
-  user: "user",
+  user: "root",
   database: "mydb",
-  password: "password"
+  password: "Y9b0@#^6IP_0952?FM?iK%%lL1JyZOYE"
+});
+
+app.get("/api/*", (req, res) => {
+  let query = `SELECT * FROM Phone WHERE company = '${req.query.company
+    }';`;
+
+  conn.query(query, (err, result, field) => {
+    console.log(err);
+    console.log(result);
+    err !== null ?
+      res.send({ status: 404, data: err }) :
+      res.send({ status: 200, data: result })
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/" + "index.html");
 });
 
 conn.connect(function (err) {
@@ -21,18 +45,6 @@ conn.connect(function (err) {
   else {
     console.log("Подключение к серверу MySQL успешно установлено");
   }
-});
-
-let query = "SELECT * FROM Phone WHERE company = 'Apple';";
-
-conn.query(query, (err, result, field) => {
-  console.log(err);
-  console.log(result);
-  console.log(field);
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/" + "index.html");
 });
 
 app.listen(port, () => {
